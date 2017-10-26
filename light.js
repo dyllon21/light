09622e7c0033ddeb396d9a8a98ae4de0d480b373
Dyllon <isaacsdyllon123@gmail.com>
2017-10-26 16:21:41 +0200

    created a class to control arduino and implemented a blink method

--- light.js
+++ light.js
@@ -1,52 +1,44 @@
-//requires johnny-five module
-
 var five = require("johnny-five");
 
 // Create an instance of the Board class - referring to the Arduino Micro Controller 'board'
 var board = new five.Board();
 
-board.on('ready', function(){
-    console.log('board ready');
+board.on('ready', function() {
+  console.log('board ready!');
+  var pin = new five.Pin(7);
+  var led = new five.Led(7);
 
-		var pin = new five.Pin(13);
+//   var on_count = 0;
+//
+// for (var i = 0; i < led.length; i++) {
+//   led[i];
+//
+//   if(led > 0){
+// return on_count++;
+//
+//   }
+// }
+  var LedLight = function(){
 
-        // the light will switch on
-        pin.high();
+    if(this.on){
+      return  counter ++;
+    }
 
-		// the light will switch off after 3 seconds
-		setTimeout(function(){
-	        pin.low()
-		}, 3000);
+    this.on = function(){
+      led.on();
+    }
+    this.off = function(){
+      led.off();
+    }
+    this.blink = function(){
+    setTimeout(function() {
+      led.blink();
+    }, 1000);
+  }
+
+
+  }
+  var light = new LedLight(7);
+  light.on();
+
 });
-console.log('\nWaiting for device to connect...');
-
-// var five = require('johnny-five'),
-// //creating new board object
-// board = new five.Board(),
-// led,
-// toggleState = false;
-//
-// //function for the board when it is ready to recieve;
-// board.on("ready", function(){
-//   console.log('board ready');
-//   led = new five.Led(13);
-// // led.blink(1000);
-//
-//   setInterval(toggleLED, 1000);
-//
-//   function toggleLED(){
-//     toggleState = !toggleState;
-//     if(toggleState) led.on();
-//     else led.off();
-//   }
-// });
-//uses toggleLed function:
-//   setInterval(toggleLed, 200);
-//
-//   function toggleLed(){
-//     toggleState = toggleState;//change from on to off
-//
-//     if(toggleState) Led.on();
-//     else Led.off();
-//   }
-// });
