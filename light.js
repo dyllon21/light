5567b88c959282ce62f1d0a1207472ed8a41cf5d
Dyllon <isaacsdyllon123@gmail.com>
2017-10-27 16:08:51 +0200

    finished controlling arduino with my own class and instance..still busy working on an internal counter

--- light.js
+++ light.js
@@ -1,44 +1,66 @@
 var five = require("johnny-five");
 
+console.log('waiting for arduino to connect');
+
 // Create an instance of the Board class - referring to the Arduino Micro Controller 'board'
-var board = new five.Board();
+var board = new five.Board({
+  port: "/dev/ttyACM0"
+});
 
 board.on('ready', function() {
   console.log('board ready!');
   var pin = new five.Pin(7);
   var led = new five.Led(7);
 
-//   var on_count = 0;
-//
-// for (var i = 0; i < led.length; i++) {
-//   led[i];
-//
-//   if(led > 0){
-// return on_count++;
-//
-//   }
-// }
-  var LedLight = function(){
 
-    if(this.on){
-      return  counter ++;
-    }
+  var LedLight = function() {
 
-    this.on = function(){
+    this.on = function() {
       led.on();
     }
-    this.off = function(){
+    this.off = function() {
       led.off();
     }
-    this.blink = function(){
-    setTimeout(function() {
+    this.blink_fast = function() {
       led.blink();
-    }, 1000);
-  }
-
-
+      setInterval(function() {
+        led.on();
+      }, 90);
+    }
+    this.blink_slow = function() {
+      led.blink();
+      setInterval(function() {
+        led.on();
+      }, 5000);
+      setTimeout(function() {
+        led.blink();
+      }, 10000);
+    }
   }
   var light = new LedLight(7);
   light.on();
+  // light.off();
+  // light.blink_fast();
+  // light.blink_slow();
+
+  var counter = 0;
+
+  setInterval(function() {
+    console.log(counter);
+    counter++
+    if (counter === light) {
+      // counter++;
+      light.on();
+    } else {
+      light.off();
+    }
+  }, 1000);
+  // }else if(counter === 5){
+  //   counter++;
+  //   led.blink();
+  // }else if (counter === 7){
+  //   counter++;
+  //   led.off();
+  // }
 
 });
